0c34990691e25ca4f3898373fa07703d87667a1d
sagarvirani <sagarvirani@gmail.com>
2023-01-13 15:52:07 +0530

    Node fundamentals (custom and built-in modules)

---
+++ Node/2-modules.js
@@ -0,0 +1,12 @@
+const names = require('./3-names.js');
+const sayHi = require('./4-utils.js');
+const data = require('./5-alternate.js')
+
+require('./6-mind-grenade.js');
+
+console.log(data);
+
+sayHi('susan');
+sayHi(names.p);
+sayHi(names.j);
+
