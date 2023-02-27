e40839c574c152c97f1ea5bd8fb41e541a4d0e27
sagarvirani <sagarvirani@gmail.com>
2023-02-27 11:14:12 +0530

    console log removed

--- Misc/360 image/app.js
+++ Misc/360 image/app.js
@@ -1,8 +1,6 @@
 const range = document.getElementById("range");    
 const main_img = document.getElementById("main");
 
-console.log(range, main_img);
-
 range.addEventListener('input', (event)=> {
     //console.log(event.target.value); 
     main_img.innerHTML = '<img src="images/'+event.target.value+'.jpg">'
